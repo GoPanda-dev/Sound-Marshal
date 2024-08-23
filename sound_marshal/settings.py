8e0b2985999874bb8827456d6657358fc73b4622
Joshua Howard <iamjoshhoward@gmail.com>
2024-08-23 14:48:29 -0400

    25th Commit - Add Allowed host dev.soundly.in

--- sound_marshal/settings.py
+++ sound_marshal/settings.py
@@ -19,7 +19,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SECRET_KEY = 'django-insecure-xilcnhd37a3!jg(xa-b7)qk+yzutg&iv5&30+%4d62f8bhok4('
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.61', 'soundmarshal.azurewebsites.net', 'www.soundmarshal.azurewebsites.net']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.61', 'soundmarshal.azurewebsites.net', 'www.soundmarshal.azurewebsites.net', 'dev.soundly.in']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://soundmarshal.azurewebsites.net',
